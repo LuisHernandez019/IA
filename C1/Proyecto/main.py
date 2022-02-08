3c6d306044a4a280235c2b7416575f249cd2e311
Luis Hernandez <uwu.luishernandez@gmail.com>
2022-02-07 21:50:44 -0600

    Update UI Project

--- C1/Proyecto/main.py
+++ C1/Proyecto/main.py
@@ -1,103 +1,101 @@
+import elitism
+import queens
 import random
 import array
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import elitism
-import queens
 from deap import base
 from deap import creator
 from deap import tools
+from tkinter import Button, StringVar, Tk, Label, Entry, font
 
-# problem constants:
-NUM_OF_QUEENS = 16
-
-# Genetic Algorithm constants:
-POPULATION_SIZE = 300
-MAX_GENERATIONS = 100
-HALL_OF_FAME_SIZE = 30
-P_CROSSOVER = 0.9  # probability for crossover
-P_MUTATION = 0.1   # probability for mutating an individual
-
-# set the random seed for repeatable results
-RANDOM_SEED = 42
-random.seed(RANDOM_SEED)
-
-# create the desired N-
-nQueens = queens.NQueensProblem(NUM_OF_QUEENS)
-
-toolbox = base.Toolbox()
-
-# define a single objective, minimizing fitness strategy:
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-
-# create the Individual class based on list of integers:
-creator.create("Individual", array.array, typecode='i', fitness=creator.FitnessMin)
-
-# create an operator that generates randomly shuffled indices:
-toolbox.register("randomOrder", random.sample, range(len(nQueens)), len(nQueens))
-
-# create the individual creation operator to fill up an Individual instance with shuffled indices:
-toolbox.register("individualCreator", tools.initIterate, creator.Individual, toolbox.randomOrder)
-
-# create the population creation operator to generate a list of individuals:
-toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
-
-
-# fitness calculation - compute the total distance of the list of cities represented by indices:
-def getViolationsCount(individual):
-    return nQueens.getViolationsCount(individual),  # return a tuple
-
-
-toolbox.register("evaluate", getViolationsCount)
-
-
-# Genetic operators:
-toolbox.register("select", tools.selTournament, tournsize=2)
-toolbox.register("mate", tools.cxUniformPartialyMatched, indpb=2.0/len(nQueens))
-toolbox.register("mutate", tools.mutShuffleIndexes, indpb=1.0/len(nQueens))
-
-
-# Genetic Algorithm flow:
 def main():
+    NUM_OF_QUEENS = int(field_reinas.get())
+    POPULATION_SIZE = int(field_poblacion.get())
+    MAX_GENERATIONS = int(field_generaciones.get())
+    P_MUTATION = float(field_mutar_g.get())
+    HALL_OF_FAME_SIZE = 30
+    P_CROSSOVER = 0.9
+    RANDOM_SEED = 42
 
-    # create initial population (generation 0):
+    random.seed(RANDOM_SEED)
+
+    nQueens = queens.NQueensProblem(NUM_OF_QUEENS)
+    toolbox = base.Toolbox()
+
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    creator.create("Individual", array.array, typecode='i', fitness=creator.FitnessMin)
+    toolbox.register("randomOrder", random.sample, range(len(nQueens)), len(nQueens))
+    toolbox.register("individualCreator", tools.initIterate, creator.Individual, toolbox.randomOrder)
+    toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
+
+    def getViolationsCount(individual):
+        return nQueens.getViolationsCount(individual),
+
+    toolbox.register("evaluate", getViolationsCount)
+    toolbox.register("select", tools.selTournament, tournsize=2)
+    toolbox.register("mate", tools.cxUniformPartialyMatched, indpb=2.0/len(nQueens))
+    toolbox.register("mutate", tools.mutShuffleIndexes, indpb=1.0/len(nQueens))
+
     population = toolbox.populationCreator(n=POPULATION_SIZE)
-
-    # prepare the statistics object:
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("min", np.min)
     stats.register("avg", np.mean)
 
-    # define the hall-of-fame object:
     hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
-
-    # perform the Genetic Algorithm flow with hof feature added:
-    population, logbook = elitism.eaSimpleWithElitism(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION,
-                                              ngen=MAX_GENERATIONS, stats=stats, halloffame=hof, verbose=True)
-
-    # print hall of fame members info:
-    print("- Best solutions are:")
-    for i in range(HALL_OF_FAME_SIZE):
-        print(i, ": ", hof.items[i].fitness.values[0], " -> ", hof.items[i])
-
-    # plot statistics:
+    population, logbook = elitism.eaSimpleWithElitism(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION, ngen=MAX_GENERATIONS, stats=stats, halloffame=hof, verbose=True)
     minFitnessValues, meanFitnessValues = logbook.select("min", "avg")
+
     plt.figure(1)
     sns.set_style("whitegrid")
     plt.plot(minFitnessValues, color='red')
     plt.plot(meanFitnessValues, color='green')
-    plt.xlabel('Generation')
-    plt.ylabel('Min / Average Fitness')
-    plt.title('Min and Average fitness over Generations')
-
-    # plot best solution:
+    plt.xlabel('Generación')
+    plt.ylabel('Fitness Mínimo / Promeido')
+    plt.title('Fitness mínimo y promedio alrededor de las generaciones.')
     sns.set_style("whitegrid", {'axes.grid' : False})
     nQueens.plotBoard(hof.items[0])
-
-    # show both plots:
     plt.show()
 
-
 if __name__ == "__main__":
-    main()
+    root = Tk()
+    root.geometry('450x320')
+    root.title('[193269/193291] Problema de las N reinas')
+    root.configure(bg='#2A0C4E')
+    
+    fuente = font.Font(size=13, font='Helvetica 10 bold')
+    background = '#2A0C4E'
+
+    test_num_gen = StringVar()
+    test_num_gen.set('8')
+    label_generaciones = Label(root,text='Número de reinas', width=21, height=1, font=fuente, background=background, fg='white')
+    label_generaciones.place(x=40,y=50)
+    field_reinas = Entry(root, width=18, font=fuente, textvariable=test_num_gen)
+    field_reinas.place(x=60,y=80)
+    
+    test_num_gen = StringVar()
+    test_num_gen.set('100')
+    label_generaciones = Label(root,text='Número de generaciones', width=21, height=1, font= fuente, background=background, fg='white')
+    label_generaciones.place(x=230,y=50)
+    field_generaciones = Entry(root, width=18, font=fuente, textvariable=test_num_gen)
+    field_generaciones.place(x=250,y=80)
+
+    test_num_poblacion = StringVar()
+    test_num_poblacion.set('300')
+    label_poblacion = Label(root,text='Tamaño de la población', width=20, height=1, font= fuente, background=background, fg='white')
+    label_poblacion.place(x=40,y=130)
+    field_poblacion = Entry(root, width=18, font=fuente, textvariable=test_num_poblacion)
+    field_poblacion.place(x=60,y=160)
+
+    test_prob_g = StringVar()
+    test_prob_g.set('0.1')
+    label_mutar_g = Label(root,text='Probabilidad de mutación', width=30, height=1, font= fuente, background=background, fg='white')
+    label_mutar_g.place(x=195,y=130)
+    field_mutar_g = Entry(root, width=18, font= fuente, textvariable=test_prob_g)
+    field_mutar_g.place(x=250,y=160)
+
+    btn_solucion = Button(root, text='Obtener solución', font=fuente, width=17, command=main)
+    btn_solucion.place(x=145, y=225)
+
+    root.mainloop()
